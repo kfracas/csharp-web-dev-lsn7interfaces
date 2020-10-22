c5493535bd613e3b079b60aa77d591e154785472
Kirby <kafracas@gmail.com>
2020-10-22 15:00:20 -0500

    completed exercise

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class ConeComparer : IComparer<Cone>
+    {
+        public int Compare(Cone x, Cone y)
+        {
+            double difference = x.Cost - y.Cost;
+            if (difference < 0)
+                return -1;
+            if (difference > 0)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ IceCreamShop/FlavorComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class FlavorComparer : IComparer<Flavor>
+    {
+        public int Compare(Flavor x, Flavor y)
+        {
+            int difference = x.Allergens.Count - y.Allergens.Count;
+            if (difference < 0)
+                return -1;
+            if (difference > 0)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
